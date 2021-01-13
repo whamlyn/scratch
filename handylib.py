e3ae44898aeec2dc5e61b662cb4bf5c8efec1713
whamlyn <wes.hamlyn@gmail.com>
2021-01-13 11:32:58 -0700

    Updates to library files

---
+++ handylib.py
@@ -0,0 +1,16 @@
+# sample python module
+
+def print_greeting():
+    """
+    Display a greeting on the command line
+    """
+    
+    print("Hi! How are you doing?")
+
+
+def sum_two_nums(x1, x2):
+    """
+    Add two numbers and return their sum.
+    """
+    val = x1 + x2
+    return val
